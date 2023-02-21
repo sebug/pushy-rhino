a6d05087309fc504a0f66806a153ff2cdf4878ad
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-02-21 19:12:33 +0000

    Adding messages html

--- src/sw.js
+++ src/sw.js
@@ -15,7 +15,8 @@ self.addEventListener('install', (event) => {
 	    'img/icon96.png',
 	    'img/icon144.png',
 	    'img/icon168.png',
-	    'img/icon192.png'
+	    'img/icon192.png',
+        'messages.html'
         ])
     );
 });
@@ -28,7 +29,20 @@ const cacheFirst = async (request) => {
     return fetch(request);
 };
 
+const fillInTemplate(request) {
+    let response = await cacheFirst(request);
+
+    if (request.url && request.url.toLowerCase().indexOf('messages.html') >= 0)
+    {
+        const responseText = await response.text();
+        responseText = responseText.replace('<!-- messages-placeholder -->', '<ul><li>First Message</li></ul>');
+        response = new Response(responseText, response);
+    }
+
+    return response;
+}
+
 self.addEventListener('fetch', (event) => {
     console.log(event.request);
-    event.respondWith(cacheFirst(event.request));
+    event.respondWith(fillInTemplate(event.request));
 });
