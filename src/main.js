a0fe3c97648350e19951f3d382521b4f4acf8183
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-02-21 17:30:48 +0000

    With a service worker

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log(navigator);
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register('/sw.js', {
+                scope: "/"
+            });
+            if (registration.installing) {
+                console.log('installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with {error}`);
+        }
+    }
+};
+
+registerServiceWorker();
---
+++ src/sw.js
@@ -0,0 +1,15 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open('v1');
+    await cache.addAll(resources);
+};
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            '/',
+            '/index.html',
+            'main.js',
+            'style.css'
+        ])
+    );
+});
