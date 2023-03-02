3ca93454be6193baaae9255bd03dc793f1602c67
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-02 11:24:50 +0000

    Trying to log and throw

--- api/RegisterForPushNotificationTrigger/index.js
+++ api/RegisterForPushNotificationTrigger/index.js
@@ -2,34 +2,39 @@ const { TableServiceClient, AzureNamedKeyCredential, TableClient } = require("@a
 
 // insert the endpoint into azure tables - check whether this works
 async function insertEndpoint(endpoint, context) {
-    const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
-    const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
-    const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
-
-    const url = 'https://' + account + '.table.' + suffix;
-
-    const credential = new AzureNamedKeyCredential(account, accountKey);
-    const serviceClient = new TableServiceClient(
-        url,
-        credential
-    );
-
-    const tableName = 'endpoints';
-    await serviceClient.createTable(tableName, {
-        onResponse: (response) => {
-            if (response.status === 409) {
-                context.log('Table endpoints already exists');
+    try {
+        const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
+        const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
+        const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
+    
+        const url = 'https://' + account + '.table.' + suffix;
+    
+        const credential = new AzureNamedKeyCredential(account, accountKey);
+        const serviceClient = new TableServiceClient(
+            url,
+            credential
+        );
+    
+        const tableName = 'endpoints';
+        await serviceClient.createTable(tableName, {
+            onResponse: (response) => {
+                if (response.status === 409) {
+                    context.log('Table endpoints already exists');
+                }
             }
-        }
-    });
-    const tableClient = new TableClient(url, tableName, credential);
-    let entity = {
-        partitionKey: "Prod",
-        rowKey: endpoint,
-        endpoint: endpoint,
-        status: 'active'
-    };
-    await tableClient.upsertEntity(entity);
+        });
+        const tableClient = new TableClient(url, tableName, credential);
+        let entity = {
+            partitionKey: "Prod",
+            rowKey: endpoint,
+            endpoint: endpoint,
+            status: 'active'
+        };
+        await tableClient.upsertEntity(entity);
+    } catch (err) {
+        context.log(err);
+        throw err;
+    }
 };
 
 module.exports = async function (context, req) {
