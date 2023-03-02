15f1a777461cf042407a1e07e2de5dc71c99dc93
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-02 15:17:54 +0000

    Only catching once

--- api/SendPushNotificationTrigger/index.js
+++ api/SendPushNotificationTrigger/index.js
@@ -3,57 +3,52 @@ const webpush = require('web-push');
 const { TableServiceClient, AzureNamedKeyCredential, TableClient } = require("@azure/data-tables");
 
 async function sendNotification(context, message, publicKey, privateKey) {
-    try {
-        const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
-        const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
-        const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
-    
-        const url = 'https://' + account + '.table.' + suffix;
+    const account = process.env.TABLES_STORAGE_ACCOUNT_NAME;
+    const accountKey = process.env.TABLES_PRIMARY_STORAGE_ACCOUNT_KEY;
+    const suffix = process.env.TABLES_STORAGE_ENDPOINT_SUFFIX;
 
-        const urlSafePublicKey = publicKey.replaceAll('+', '-')
-            .replaceAll('/', '_');
-        const urlSafePrivateKey = privateKey.replaceAll('+', '-')
+    const url = 'https://' + account + '.table.' + suffix;
+
+    const urlSafePublicKey = publicKey.replaceAll('+', '-')
         .replaceAll('/', '_');
-    
-        const credential = new AzureNamedKeyCredential(account, accountKey);
-        const serviceClient = new TableServiceClient(
-            url,
-            credential
-        );
-    
-        const tableName = 'endpoints';
-        await serviceClient.createTable(tableName, {
-            onResponse: (response) => {
-                if (response.status === 409) {
-                    context.log('Table endpoints already exists');
-                }
+    const urlSafePrivateKey = privateKey.replaceAll('+', '-')
+    .replaceAll('/', '_');
+
+    const credential = new AzureNamedKeyCredential(account, accountKey);
+    const serviceClient = new TableServiceClient(
+        url,
+        credential
+    );
+
+    const tableName = 'endpoints';
+    await serviceClient.createTable(tableName, {
+        onResponse: (response) => {
+            if (response.status === 409) {
+                context.log('Table endpoints already exists');
             }
-        });
-        const tableClient = new TableClient(url, tableName, credential);
-        let entitiesIter = tableClient.listEntities();
-        let entityItem = await entitiesIter.next();
-        while (entityItem && !entityItem.done && entityItem.value) {
-            const entity = entityItem.value;
-            let options = {
-                vapidDetails: {
-                    subject: 'https://ashy-sky-0a409fd03.2.azurestaticapps.net/',
-                    publicKey: urlSafePublicKey,
-                    privateKey: urlSafePrivateKey
-                }
-            };
-            let pushSubscription = {
-                endpoint: entity.endpoint,
-                keys: {
-                    authKey: entity.authKey,
-                    p256dh: entity.p256dh
-                }
-            };
-            webpush.sendNotification(pushSubscription, message, options);
-            entityItem = await entitiesIter.next();
         }
-    } catch (err) {
-        context.log(err);
-        throw err;
+    });
+    const tableClient = new TableClient(url, tableName, credential);
+    let entitiesIter = tableClient.listEntities();
+    let entityItem = await entitiesIter.next();
+    while (entityItem && !entityItem.done && entityItem.value) {
+        const entity = entityItem.value;
+        let options = {
+            vapidDetails: {
+                subject: 'https://ashy-sky-0a409fd03.2.azurestaticapps.net/',
+                publicKey: urlSafePublicKey,
+                privateKey: urlSafePrivateKey
+            }
+        };
+        let pushSubscription = {
+            endpoint: entity.endpoint,
+            keys: {
+                authKey: entity.authKey,
+                p256dh: entity.p256dh
+            }
+        };
+        webpush.sendNotification(pushSubscription, message, options);
+        entityItem = await entitiesIter.next();
     }
 }
 
@@ -80,7 +75,7 @@ module.exports = async function (context, req) {
     } catch (err) {
         context.res = {
             status: 500,
-            body: err
+            body: err || 'no content in exception'
         };
     }
 };
