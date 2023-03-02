c98021ed2d9d02f9e50d56099841e36d9e82f7f3
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-03-02 13:09:45 +0000

    Showing error message

--- api/SendPushNotificationTrigger/index.js
+++ api/SendPushNotificationTrigger/index.js
@@ -1,17 +1,24 @@
 import { parse } from 'qs';
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    try {
+        context.log('JavaScript HTTP trigger function processed a request.');
 
-    const parsedData = parse(req.rawBody);
-
-    const message = parsedData && parsedData.message;
-    const responseMessage = message
-        ? ("Received message " + message)
-        : "This HTTP triggered function executed successfully. Pass a message in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        const parsedData = parse(req.rawBody);
+    
+        const message = parsedData && parsedData.message;
+        const responseMessage = message
+            ? ("Received message " + message)
+            : "This HTTP triggered function executed successfully. Pass a message in the request body for a personalized response.";
+    
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };
+    } catch (err) {
+        context.res = {
+            status: 500,
+            body: err
+        };
+    }
 }
